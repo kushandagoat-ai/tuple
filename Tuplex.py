a32a4acdc44e282786898018b1a36cd67bab666a
kushandagoat-ai <kushandagoat@gmail.com>
2026-05-10 17:01:43 -0700

    Add files via upload

---
+++ Tuplex.py
@@ -0,0 +1,18 @@
+tuplex = ("Tuple", False, 3, 2, 1 )
+print(tuplex)
+
+tuplex = (9,4,6,2,4,5,6,7)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple = (50,10,60,50,50,50,50,50,50)
+print(tuplex.count(50))
+
+tuplex = (2,4,3,5,3,3,6,8,9,0)
+_slice = tuplex[3:5]
+print(_slice)
+
+_slice = tuplex[:6]
+print(_slice)
